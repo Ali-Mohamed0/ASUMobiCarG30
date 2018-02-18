d105e654bc05475b179220202f53bd7f68a5f8cb
Ali-Mohamed0 <a.omar125242@gmail.com>
2018-02-18 20:51:19 +0200

    add first arduino example

---
+++ Arduino examples/simple on off led.ino
@@ -0,0 +1,12 @@
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+ void loop() {
+  digitalWrite(13, HIGH);   
+  delay(1000);               
+  digitalWrite(13, LOW);     
+  delay(1000);               
+}
