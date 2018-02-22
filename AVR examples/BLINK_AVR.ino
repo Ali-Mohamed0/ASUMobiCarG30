82f1f76ac21b0f60baaeccbb6b660b1ba9cacbed
MayarGamal <36451437+MayarGamal@users.noreply.github.com>
2018-02-22 03:31:51 +0200

    Add files via upload

---
+++ AVR examples/BLINK_AVR.ino
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+#include <avr/delay.h> 
+int main(void) { 
+  DDRB = 0b00000001;  
+while(1)     {
+  PORTB = 0b00000001; 
+  _delay_ms(500);      
+  PORTB = 0b00000000; 
+  _delay_ms(500); 
+  }
+  return 0; 
+ }
+
