c0531dbe42e904e684de49562a91aa40b08eddf0
AbdelazimMohamed <36339524+AbdelazimMohamed@users.noreply.github.com>
2018-02-22 23:26:42 +0200

    Add files via upload

---
+++ Arduino examples/SHEET_1__EX.1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(1,HIGH);
+delay(500);
+digitalWrite(1,LOW);
+delay(1500);
+
+}
