c10714443fe289ba49675286990e0c39471a664d
Ali Ahmed <31663309+Ali2030@users.noreply.github.com>
2018-02-23 04:42:30 +0200

    Add files via upload

---
+++ Arduino examples/LED_Brightness_Control.ino
@@ -0,0 +1,16 @@
+#define ledPin 13
+int i=255;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(ledPin,i);
+delay(500);
+i=i-55;
+if(i<0)
+i=255;
+}
