36286b8006f310c365d9bc513c5bb137da66fa24
omarahmed1111 <30423288+omarahmed1111@users.noreply.github.com>
2018-02-24 16:05:20 +0200

    connection
    removed the previous connection as it can be done from the mobile and establish directly a thread to connect to communicate via bluetooth socket and added the permissions

---
+++ Mobile app/control/app/src/main/java/com/example/android/control/BluetoothConnectionService.java
@@ -0,0 +1,53 @@
+package com.example.android.control;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by omar 2 on 2/24/2018.
+ */
+
+public class BluetoothConnectionService {
+    private static final String TAG = "BluetoothConnectionServ";
+    private static final String appName = "MYAPP";
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+    private final BluetoothAdapter mBluetoothAdapter;
+    Context mContext;
+    private AcceptThread mInsecureAcceptThread;
+    public BluetoothConnectionService(Context context) {
+        mContext = context;
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    }
+
+    private class AcceptThread extends Thread {
+
+        // The local server socket
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread(){
+            BluetoothServerSocket tmp = null;
+
+            // Create a new listening server socket
+            try{
+                tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName, MY_UUID_INSECURE);
+
+                Log.d(TAG, "AcceptThread: Setting up Server using: " + MY_UUID_INSECURE);
+            }catch (IOException e){
+                Log.e(TAG, "AcceptThread: IOException: " + e.getMessage() );
+            }
+
+            mmServerSocket = tmp;
+        }
+
+
+
+    }
+}
--- Mobile app/control/app/src/main/java/com/example/android/control/MainActivity.java
+++ Mobile app/control/app/src/main/java/com/example/android/control/MainActivity.java
@@ -15,73 +15,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    BluetoothAdapter B_Adapter;
-    BroadcastReceiver mBroadcastReceiver1;
-    private static final String TAG = "MainActivity";
 
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(B_Adapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, B_Adapter.ERROR);
-
-                switch(state){
-                    case BluetoothAdapter.STATE_OFF:
-                        Log.d(TAG, "onReceive: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING OFF");
-                        break;
-                    case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE ON");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING ON");
-                        break;
-                }
-
-            }
-        }
-    };
-    @Override
-    protected void onDestroy() {
-        Log.d(TAG, "onDestroy: called.");
-        super.onDestroy();
-        unregisterReceiver(mBroadcastReceiver1);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button connect_btn = (Button) findViewById(R.id.co);
-
-        B_Adapter = BluetoothAdapter.getDefaultAdapter();
-        connect_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enableDisable();
-            }
-        });
-    }
-
-    private void enableDisable() {
-        if(B_Adapter == null){
-            Log.d(TAG, "enableDisable: device does not have bluetooth");
-        }
-        if(!B_Adapter.isEnabled()){
-            Intent enable_btn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enable_btn);
-
-            IntentFilter btIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastReceiver1,btIntent);
-        }
-        if(B_Adapter.isEnabled()){
-            B_Adapter.disable();
-            IntentFilter btIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastReceiver1,btIntent);
-        }
 
     }
+
 }
