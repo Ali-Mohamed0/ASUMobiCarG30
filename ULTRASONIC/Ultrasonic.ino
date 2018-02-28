1e5ebc6200c858d85fb64dd74a02c204b68e008a
Ali Ahmed <31663309+Ali2030@users.noreply.github.com>
2018-02-28 02:33:47 +0200

    Add files via upload

---
+++ ULTRASONIC/Ultrasonic.ino
@@ -0,0 +1,27 @@
+const int trigPin = 9;     // defines pins numbers
+const int echoPin = 10;
+
+long duration;             // defines variables
+int distance;
+
+void setup() {
+pinMode(trigPin, OUTPUT);     // Sets the trigPin as an Output
+pinMode(echoPin, INPUT);     // Sets the echoPin as an Input
+Serial.begin(9600);         // Starts the serial communication
+}
+
+void loop() {
+
+digitalWrite(trigPin, LOW);       // Clears the trigPin
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);       // Sets the trigPin on HIGH state for 10 micro seconds
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH);    // Reads the echoPin, returns the sound wave travel time in microseconds
+
+distance= duration*0.034/2;             // Calculating the distance
+
+Serial.print("Distance: ");              // Prints the distance on the Serial Monitor
+Serial.println(distance);
+}
