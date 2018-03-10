8565b0bca0b28be05a18de9ac24b91cae4252288
MayarGamal <1501532@eng.asu.edu.eg>
2018-03-10 04:54:29 +0200

    3'alban fe mistakes

--- Car code/carCode/carCode.ino
+++ Car code/carCode/carCode.ino
@@ -5,14 +5,38 @@ void forward(int velocity);
 void back(int velocity);
 void right(int velocity);
 void left(int velocity);
+void stopcar();
+void inc_speed();
+void dec_speed();
 void ultrasonic();
 
-int main (void){
-   DDRD = 0b01100000 ; //setting D5 & D6 as an output
-   DDRB = 0b00000110 ; // setting B1& B2 as an output
-  }
-  
 void setup() {
+  DDRD |= 1 << PIND5;  //setting D5 as an output
+  DDRD |= 1 << PIND6;  //setting D6 as an output
+  DDRB |= 1 << PINB1;  // setting B1 as an output
+  DDRB |= 1 << PINB2;  // setting B2 as an output
+
+  //---setting fast pwm mode bits on TC0 Control Register A ---// 
+    TCCR0A |=1<<COM0A1 ;
+    TCCR0A |=1<<WGM00 ;
+    TCCR0A |=1<<WGM01 ;
+    //--------------------------------------------//
+    
+   //---setting fast pwm mode bits on TC0 Control Register B ---//
+      TCCR0B |=1<<CS00 ; //no prescalling
+    //-----------------------------------------------------------// 
+
+        //---setting fast pwm mode bits on TC1 Control Register A ---// 
+    TCCR0A |=1<<COM1A1 ;
+    TCCR0A |=1<<WGM10 ;
+    TCCR0A |=1<<WGM12 ;
+    //--------------------------------------------//
+
+
+   //---setting fast pwm mode bits on TC1 Control Register B ---//
+      TCCR0B |=1<<CS10 ; //no prescalling
+    //-----------------------------------------------------------// 
+
 }
 
 void loop() {
@@ -23,24 +47,61 @@ void ultrasonic(){
   
   }
 
-
 void forward(int velocity){
-  PORTD = 0b00100000 ; //D5 high (M1F)
-  PORTB = 0b00000010 ; //B1 high (M2F)
+  OCR0A = velocity ;
+  OCR1A = velocity ;  //law el velocity hategy mn el app 
+  PORTD |= 1 << PIND5 ; //D5 high (M1F)
+  PORTB |= 1 << PINB1 ; //B1 high (M2F)
   }
+  
 void back(int velocity){
-  PORTD = 0b01000000 ; //D6 high (M1B)
-  PORTB = 0b00000100 ; //B2 high (M2B)
+  OCR0A = velocity ;
+  PORTD |= 1 << PIND6 ; //D6 high (M1B)
+  PORTB |= 1 << PINB2 ; //B2 high (M2B)
   }
+  
 void right(int velocity){
-  PORTD = 0b00000000 ; //M1 STOPS 
-  PORTB = 0b00000010 ; //B1 high (M2F)
+  OCR0A = velocity ;
+  OCR0B = velocity ; 
+  PORTD &= 0 << PIND5 ; //M1F STOPS 
+  PORTD &= 0 << PIND6 ; //M1B STOPS 
+  PORTB |= 1 << PINB1 ; //B1 high (M2F)
   }
+  
 void left(int velocity){
-  PORTD = 0b00100000 ; //D5 high (M1F)
-  PORTB = 0b00000010 ; //M2 STOPS
+  OCR0B = velocity ;
+  PORTD |= 1 << PIND5 ; //D5 high (M1F)
+  PORTB &= 0 << PINB1 ; //M2F STOPS 
+  PORTB &= 0 << PINB2 ; //M2B STOPS 
   }
+  
 void stopcar(){
-  PORTD = 0b00000000 ; //M1 STOPS
-  PORTB = 0b00000010 ; //M2 STOPS
+  PORTD &= 0 << PIND5 ; //M1F STOPS 
+  PORTD &= 0 << PIND6 ; //M1B STOPS
+  PORTB &= 0 << PINB1 ; //M2F STOPS 
+  PORTB &= 0 << PINB2 ; //M2B STOPS 
+  }
+
+  //law el speed control hatkon mn 2 buttons inc & dec only 
+void inc_speed(){
+  
+  OCR0A +=50; 
+  OCR1A +=50;
+  OCR0B +=50; 
+  OCR1B +=50;  
+  
+  if (OCR0A > 255){
+      OCR0A = 255 ; 
+      }
+  }
+  
+void dec_speed(){
+  OCR0A -=50; 
+  OCR1A -=50;
+  OCR0B -=50; 
+  OCR1B -=50;
+  
+   if (OCR0A < 0 ){
+     OCR0A = 0 ; 
+      }
   }  
