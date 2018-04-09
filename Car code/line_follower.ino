4150c16e8b732655c2749d594ccb2eab48bbd1fa
Mai <36275889+ElecMai@users.noreply.github.com>
2018-04-10 00:39:29 +0300

    Add files via upload

---
+++ Car code/line_follower.ino
@@ -0,0 +1,41 @@
+
+int sen1=4;
+int sen2=6;
+int Left= HIGH;
+int Right=HIGH;
+
+
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(sen1,INPUT); 
+   pinMode(sen2,INPUT);
+   
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  Right=digitalRead(sen1);
+
+  Left=digitalRead(sen2);
+  
+  if (Right==HIGH & Left==HIGH)
+  {
+    //forward();
+  }
+
+  else if(Right==LOW & Left==HIGH)
+  {
+    //right();
+  }
+  else if(Right==HIGH & Left==LOW)
+  {
+    //left();
+  }
+  else
+  {
+    //stopcar();
+  }
+  
+}
+
+
